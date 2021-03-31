e97704b1fc304ae938115bfd6233cad60dc01b0f
UncleDep <pedrodglira@gmail.com>
2021-03-30 22:02:57 -0300

    adicionadas relações com tarefa

--- validar-aviso.php
+++ validar-aviso.php
@@ -133,6 +133,7 @@ if($result->num_rows > 0){
 }
 
 $autor = $_SESSION['id'];
+$last_id = 0;
 
 if($existe_disciplinas and $existe_turmas){
     $_SESSION["id"];
@@ -141,9 +142,38 @@ if($existe_disciplinas and $existe_turmas){
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
+        $last_id = $conn->insert_id;
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
+
+    foreach($turmas as $x){
+        if($x != "/end"){
+            $sql = "INSERT INTO ATIVIDADE_TURMA VALUES(NULL, '$last_id', '$x');";
+            echo $sql;
+
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+
+    foreach($disciplinas as $x){
+        if($x != "/end"){
+            $sql = "INSERT INTO ATIVIDADE_DISCIPLINA VALUES(NULL, '$last_id', '$x')";
+            echo $sql;
+        }
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    
 
     header("Location: home.php");
 }
